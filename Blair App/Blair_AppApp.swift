26605666ba0e56fe4765f24cfb904c14a2207698
leewilled <khaliwilli@gmail.com>
2024-01-22 10:34:15 -0500

    Initial Commit

---
+++ Blair App/Blair_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blair_AppApp.swift
+//  Blair App
+//
+//  Created by Achsah Gubena on 1/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Blair_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
